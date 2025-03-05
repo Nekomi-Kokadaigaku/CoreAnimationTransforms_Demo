af62cf57e8edcf15fa2c4dcfb4b9a69f6abbc4bc
Erica-Iris <ediath462@gmail.com>
2025-03-05 15:33:30 +0800

    Initial Commit

---
+++ CoreAnimationTransforms_Demo/CoreAnimationTransforms_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreAnimationTransforms_DemoApp.swift
+//  CoreAnimationTransforms_Demo
+//
+//  Created by Iris on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct CoreAnimationTransforms_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
